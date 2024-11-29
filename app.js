0a2b6e71806b71b7b5170a4f50342064dc7ac7c9
Vaibhavgoel <vaibhavgoel474@gmail.com>
2024-11-29 21:05:27 +0530

    new files

--- app.js
+++ app.js
@@ -22,13 +22,17 @@ const mapToken = 'pk.eyJ1Ijoic2FydGhhazEyMSIsImEiOiJjbHhsazF0bXIwMThhMmxzM2NoeXR
 const geocodingClient = geocoding({accessToken : mapToken});
 const RateLimit = require('express-rate-limit');
 const helmet = require('helmet');
-const { type } = require('os');
 const http = require('http');
-const socketIo = require('socket.io');
+const { Server } = require('socket.io');
+
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
-
+const io = new Server(server, {
+    cors: {
+        origin: '*', // Replace with allowed origin(s)
+        methods: ['GET', 'POST'],
+    },
+});
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
@@ -37,25 +41,8 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, 'public')));
 // app.use(bodyParser.urlencoded({ extended: true }));
-
-
-
-
-io.on('connection', (socket) => {
-    console.log('User connected:', socket.id);
-
-
-    socket.on('disconnect', () => {
-        console.log('User disconnected:', socket.id);
-    });
-});
-
-
-
-
-
-
 
 
 
@@ -82,6 +69,23 @@ app.use(session({
         httpOnly : true,
     },
 }));
+
+
+io.on('connection', (socket) => {
+    console.log('User connected:', socket.id);
+
+    socket.on("message", (msg) => {
+        io.emit("message",msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+});
+
+
+
+
 app.use(flash());
 
 main()
